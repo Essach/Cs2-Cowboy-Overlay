e290df4b427e53f5d3439de3b5904326569d6e9f
Adrian <adrian.wozny@interia.pl>
2026-04-05 22:47:13 +0200

    feat. kuń

--- overlay.py
+++ overlay.py
@@ -16,10 +16,15 @@ class Overlay(QMainWindow):
         self.initUI()
     
     def initUI(self):
-        self.wheat = QLabel(self)
-        self.wheat.setGeometry(self.screenWidth//2, self.screenHeight - 380, 556, 420)
-        self.wheat.setStyleSheet("border-image: url(wheat.png);")
 
         self.hat = QLabel(self)
         self.hat.setGeometry(0, 0, self.screenWidth, 250)
         self.hat.setStyleSheet("border-image: url(hat.png);")
+        
+        self.horse = QLabel(self)
+        self.horse.setGeometry(self.screenWidth//2 - 550, self.screenHeight- 720, 1000, 1000)
+        self.horse.setStyleSheet("border-image: url(kon.png);")
+
+        self.wheat = QLabel(self)
+        self.wheat.setGeometry(self.screenWidth//2, self.screenHeight - 380, 556, 420)
+        self.wheat.setStyleSheet("border-image: url(wheat.png);")
